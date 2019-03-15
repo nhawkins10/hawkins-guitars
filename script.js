f5110c3425ac699db8316a53c0b996c42ea4f105
Nathan Hawkins <Nathan.Hawkins@YRCFreight.com>
2019-03-15 14:54:43 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+	document.getElementById('nav-icon').classList.toggle('open');
+	document.getElementById('nav-menu').classList.toggle('open');
+}
+
+function shufflePicture(type) {
+	var counts = {
+		t: 7,
+		s: 7,
+		p: 7
+	};
+		
+	document.getElementById(type).src = 'assets/' + type + '-style/' + type + '-style-' + parseInt((Math.random() * counts[type])+1) + '.png';
+}
